d72841d308b099815b627d37f2810eccba027f2f
Nathan Klapstein <nklapste@ualberta.ca>
2020-03-27 15:24:37 -0600

    adding stubs for api

--- autotradeweb/server.py
+++ autotradeweb/server.py
@@ -14,7 +14,8 @@ from dash.dependencies import Input, Output
 from flask import Flask, render_template, send_from_directory, request, redirect, \
     url_for, abort, Response
 from flask_sqlalchemy import SQLAlchemy
-from flask_simplelogin import SimpleLogin, login_required
+from flask_simplelogin import SimpleLogin, login_required, get_username
+from flask_restx import Api, Resource, fields
 
 __log__ = getLogger(__name__)
 
@@ -27,6 +28,10 @@ DEFAULT_SQLITE_PATH = "sqlite:///autotradeweb.db"
 APP.config['SQLALCHEMY_DATABASE_URI'] = DEFAULT_SQLITE_PATH
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(APP)
+
+
+API = Api()
+API.init_app(APP)
 
 
 def validate_login(user):
@@ -277,3 +282,87 @@ def account():
 @login_required
 def history():
     return render_template("history.html")
+
+
+
+# API definitions
+
+
+class TodoDAO(object):
+    def __init__(self):
+        self.counter = 0
+        self.todos = []
+
+    def get(self, id):
+        for todo in self.todos:
+            if todo['id'] == id:
+                return todo
+        api.abort(404, "Todo {} doesn't exist".format(id))
+
+    def create(self, data):
+        todo = data
+        todo['id'] = self.counter = self.counter + 1
+        self.todos.append(todo)
+        return todo
+
+    def update(self, id, data):
+        todo = self.get(id)
+        todo.update(data)
+        return todo
+
+    def delete(self, id):
+        todo = self.get(id)
+        self.todos.remove(todo)
+
+
+DAO = TodoDAO()
+DAO.create({'task': 'Build an API'})
+DAO.create({'task': '?????'})
+DAO.create({'task': 'profit!'})
+
+todo = API.model('Todo', {
+    'id': fields.Integer(readOnly=True, description='The task unique identifier'),
+    'task': fields.String(required=True, description='The task details')
+})
+
+ns = API.namespace('todos', description='TODO operations')
+@ns.route('/')
+class TodoList(Resource):
+    '''Shows a list of all todos, and lets you POST to add new tasks'''
+    @ns.doc('list_todos')
+    @ns.marshal_list_with(todo)
+    def get(self):
+        '''List all tasks'''
+        return DAO.todos
+
+    @ns.doc('create_todo')
+    @ns.expect(todo)
+    @ns.marshal_with(todo, code=201)
+    def post(self):
+        '''Create a new task'''
+        return DAO.create(api.payload), 201
+
+
+@ns.route('/<int:id>')
+@ns.response(404, 'Todo not found')
+@ns.param('id', 'The task identifier')
+class User(Resource):
+    '''Show a single todo item and lets you delete them'''
+    @ns.doc('get_todo')
+    @ns.marshal_with(todo)
+    def get(self, id):
+        '''Fetch a given resource'''
+        return DAO.get(id)
+
+    @ns.doc('delete_todo')
+    @ns.response(204, 'Todo deleted')
+    def delete(self, id):
+        '''Delete a task given its identifier'''
+        DAO.delete(id)
+        return '', 204
+
+    @ns.expect(todo)
+    @ns.marshal_with(todo)
+    def put(self, id):
+        '''Update a task given its identifier'''
+        return DAO.update(id, api.payload)
