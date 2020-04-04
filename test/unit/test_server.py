9de8edd5b013f78e51099b0b710c51b1fa2aaf81
Nathan Klapstein <nklapste@ualberta.ca>
2020-04-04 12:56:04 -0600

    adding tests for the database bindings

--- test/unit/test_server.py
+++ test/unit/test_server.py
@@ -3,10 +3,16 @@
 
 """pytests for :mod:`.server`"""
 
+import os
+from datetime import datetime
+
 import pytest
 from sqlalchemy.exc import OperationalError
 
-from autotradeweb.server import APP
+from autotradeweb.server import APP, User, db, trading_session, trade, stock_prediction, \
+    stock_data
+
+APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('TEST_DATABASE_URI')
 
 
 @pytest.fixture(scope='module')
@@ -47,9 +53,14 @@ class TestBasicFlaskApp:
 
     def test_register_post(self, client):
         # TODO: does not work until we find way to emulate postgrelocally
+        # setup empty the user table
+        db.session.query(User).delete()
+        db.session.commit()
+
         try:
             resp = client.post("/register", data={"email": "foo", "psw": 'bar'})
-            assert resp.status_code == 200
+            assert resp.status_code == 302
+            assert "login" in resp.location
         except OperationalError:
             pass
 
@@ -67,3 +78,40 @@ class TestLoggedInBasicFlaskApp:
         as intended"""
         # TODO: implement
         assert False
+
+
+class TestDatabaseBindings:
+
+    def test_add_user(self):
+        db.session.query(User).delete()
+        db.session.commit()
+        user = User(username="foo", password="bar")
+        db.session.add(user)
+        db.session.commit()
+        assert user.id
+        assert user.to_dict()
+
+    def test_add_trading_session(self):
+        trading_session_ = trading_session(username="foo", ticker="bar", start_time=datetime.now())
+        db.session.add(trading_session_)
+        db.session.commit()
+        assert trading_session_.session_id
+        assert trading_session_.to_dict()
+        db.session.delete(trading_session_)
+        db.session.commit()
+
+    def test_get_trade(self):
+        trade_ = db.session.query(trade).first()
+        assert trade_
+        assert trade_.trade_id
+        assert trade_.to_dict()
+
+    def test_get_stock_prediction(self):
+        stock_prediction_ = db.session.query(stock_prediction).first()
+        assert stock_prediction_
+        assert stock_prediction_.stock_name
+
+    def test_get_stock_data(self):
+        stock_data_ = db.session.query(stock_data).first()
+        assert stock_data_
+        assert stock_data_.stock_name
