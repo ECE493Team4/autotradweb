367e9fadf49c72a9d2c66c7f9bb86da84d4a6cfb
Nathan Klapstein <nklapste@ualberta.ca>
2020-04-06 13:43:22 -0600

    adding mappings to the `num_trades` column in `trading_session`

--- autotradeweb/server.py
+++ autotradeweb/server.py
@@ -79,6 +79,7 @@ class trading_session(db.Model):
     ticker = db.Column(db.String(80))
     start_time = db.Column(db.DateTime())
     end_time = db.Column(db.DateTime())
+    num_trades = db.Column(db.Integer, default=0)
     is_paused = db.Column(db.Boolean(), default=False)
     is_finished = db.Column(db.Boolean(), default=False)
 
@@ -92,6 +93,7 @@ class trading_session(db.Model):
             "is_finished": self.is_finished,
             "start_time": self.start_time,
             "end_time": self.end_time,
+            "num_trades": int(self.num_trades)
         }
 
 
@@ -358,6 +360,7 @@ TRADING_SESSION = api.model(
         "is_finished": fields.Boolean(default=False),
         "start_time": fields.DateTime(),
         "end_time": fields.DateTime(),
+        "num_trades": fields.Integer(default=0),
     },
 )
 
